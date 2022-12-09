c6e43fe634d95b5590b761473cce1e47f8cf348e
kolaT1 <113561175+kolaT1@users.noreply.github.com>
2022-12-08 23:24:33 -0500

    Update InterimReportB.py

--- InterimReportB.py
+++ InterimReportB.py
@@ -1,104 +1,87 @@
 import math
-
-from spike import PrimeHub, LightMatrix, MotorPair, Motor, MotionSensor, ColorSensor, ForceSensor
+from spike import PrimeHub, LightMatrix, MotorPair, Motor, MotionSensor, ColorSensor, ForceSensor, PIDController
 from spike.control import Timer, wait_for_seconds
-import math as np
-
-import math
-
+# Import the PIDController class
+from spike.control import PIDController
+	
 # ------------------- SpikeRescue -------------
-# 
-
 motor_pair = MotorPair('C', 'D')
 motorLeft = Motor('C')
 motorRight = Motor('D')
 armMotor = Motor('B')
-
-motion_sensor = MotionSensor('')
-color_sensor = ColorSensor('')
-force_sensor = ForceSensor('')
-
+# Instantiate the sensors with the correct port names
+motion_sensor = MotionSensor()
+color_sensor = ColorSensor('E')
+force_sensor = ForceSensor('F')
 hub = PrimeHub()
-
 timer = Timer()
 timer.reset()
 lm = LightMatrix()
-
-
 # --------- Frontier 1 -------------------------
-
 # Define a function to calculate the distance between two points
 def distance(x1, y1, x2, y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
 # Define a function to calculate the next point to move to
 # using the Frontier Algorithm
 def next_point(current_x, current_y, destination_x, destination_y):
     # Calculate the distance to the destination
     d = distance(current_x, current_y, destination_x, destination_y)
-
     # If we are within a certain distance of the destination, stop
     if d < 5:
-        return current_x, current_y
 
+        return current_x, current_y
     # Otherwise, calculate the next point to move to using the Frontier Algorithm
     else:
         # Calculate the angle to the destination
         theta = math.atan2(destination_y - current_y, destination_x - current_x)
-
         # Calculate the next point to move to, based on the angle and a fixed step size
         next_x = current_x + math.cos(theta) * 10
         next_y = current_y + math.sin(theta) * 10
-
         return next_x, next_y
-
 # Define the coordinates of the starting point and the destination
 start_x = 0
 start_y = 0
 destination_x = 100
 destination_y = 100
-
 # Initialize the current position to the starting point
 current_x = start_x
 current_y = start_y
+# Create a PIDController 
+pid = PIDController(kp=1.0, ki=0.1, kd=0.5)
+# Set the target position for the PID controller
+pid.set_target(destination_x, destination_y)
 
 # Keep moving until we reach the destination
 while True:
-    # Calculate the next point to move to
-    next_x, next_y = next_point(current_x, current_y, destination_x, destination_y)
 
-    # Move the robot to the next point
-    motor_pair.move(next_x, next_y)
+    # Update the PID controller and get the control output
+    output = pid.update(current_x, current_y)
 
-    # Update the current position
-    current_x = next_x
-    current_y = next_y
+    # Use the control output to set the motor speeds
+    motorLeft.spin(output[0])
+    motorRight.spin(output[1])
+
+    # Update the current position using the motion sensor
+    current_x, current_y = motion_sensor.position()
 
     # If we have reached the destination, stop
     if current_x == destination_x and current_y == destination_y:
         break
 
-
 # -------------- Frontier 2 -------------------
-
 # Define the robot's starting position and orientation
 start = (0, 0)
 orientation = 0
-
 # Define the dimensions of the maze
 width = 10
 height = 10
-
 # Define the maze as a 2D grid
 grid = [[0 for x in range(width)] for y in range(height)]
-
 # Set the robot's starting position on the grid
 grid[start[0]][start[1]] = 1
-
 # Define the movements that the robot can make
 # (up, right, down, left)
 movements = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
 # Define the Frontier Algorithm function
 def frontier(grid, start):
     # Initialize the queue with the starting position
@@ -123,10 +106,8 @@ def frontier(grid, start):
                 grid[new_pos[0]][new_pos[1]] = 1
     # Return the visited list
     return visited
-
 # Call the Frontier Algorithm function to get the visited positions
 visited = frontier(grid, start)
-
 # Loop through the visited positions
 for pos in visited:
     # Calculate the movement to get to the next position
@@ -137,7 +118,7 @@ for pos in visited:
     robot.drive_distance(1, orientation * 90)
     # Update the robot's starting position
     start = pos
-
+    	
 # ------------------- SLAM ----------------------------
 
 # Create a Timer object to periodically update the robot's position and orientation
