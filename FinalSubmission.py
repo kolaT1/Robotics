af0f17123840149d5bca7d742eb22baf453787bc
joshfigueroa <jrfigueroa1228@gmail.com>
2022-12-16 13:14:45 -0500

    Minor progress on function Switched to using the distance sensor Still not functional

--- FinalSubmission.py
+++ FinalSubmission.py
@@ -1,26 +1,28 @@
 import math
-from spike import PrimeHub, LightMatrix, MotorPair, Motor, MotionSensor, ColorSensor, ForceSensor, PIDController
+from spike import PrimeHub, LightMatrix, MotorPair, Motor, MotionSensor, ColorSensor, DistanceSensor, #PIDController
 from spike.control import Timer, wait_for_seconds
 # Import the PIDController class
-from spike.control import PIDController
+# from spike.control import PIDController
 	
 # ------------------- SpikeRescue -------------
 motor_pair = MotorPair('C', 'D')
 motorLeft = Motor('C')
 motorRight = Motor('D')
 armMotor = Motor('B')
-force = ForceSensor('A')
+
 # Instantiate the sensors with the correct port names
-motion_sensor = MotionSensor()
 color_sensor = ColorSensor('E')
-force_sensor = ForceSensor('F')
+distance_sensor = DistanceSensor('F')
 hub = PrimeHub()
-timer = Timer()
-timer.reset()
 lm = LightMatrix()
 
+distance_sensor.light_up_all(100)
+
+timer = Timer()
+timer.reset()
+
 # Create a PIDController 
-pid = PIDController(kp=1.0, ki=0.1, kd=0.5)
+#pid = PIDController(kp=1.0, ki=0.1, kd=0.5)
 
 # -------- liftArm -------------------------
 # lifts Spike's effector for a set duration    
@@ -31,23 +33,42 @@ def liftArm(duration):
 def lowerArm(duration):
     armMotor.run_for_rotations(-0.15)
 
+# ------- spinSearch -----------------------
+# Spike will continue moving out in a spiral until
+# it gets too close to a wall. It will then stop
+
 def spinSearch():
-        motor_pair.move(1000, unit='in', steering=-15, speed=20)
-        if force.is_pressed():
-            motor_pair.stop()
-            motor_pair.move(2, unit='in', steering=-100, speed=20)
-            motor_pair.stop()
+    while True:
+        distance_sensor.wait_for_distance_farther_than(5, 'in', short_range=False)
+        motor_pair.move(1000, unit='in', steering=-30, speed=40)
+        motor_pair.stop()
+        motor_pair.move(10, unit='in', steering=-100, speed=80)
+        motor_pair.stop()
+        #rowSearch()
+
+
+# ------- spinSearch -----------------------
+# Spike will move back and forth in straight lines.
+# When a wall is reached, it will turn around and begin again.
 
 def rowSearch():
+    if (timer < timer.now()):
         motor_pair.move(100, unit='in', steering=0, speed=20)
-        if force.is_pressed():
-            motor_pair.move(2, unit='in', steering=-100, speed=20)
-            motor_pair.move(100, unit='in', steering=0, speed=20)
-            rowSearch()
+        distance_sensor.wait_for_distance_closer_than(1, 'in')
+        motor_pair.move(2, unit='in', steering=-100, speed=20)
+        motor_pair.move(100, unit='in', steering=0, speed=20)
+        rowSearch()
+        
     
 
+# ------- targetSearch ---------------------
+# Spike will check to see if the right color target is in
+# front of it. If it is, it will lift it up and beep 3 times.
+# Otherwise, it will beep once.
+
 def targetSearch():
-    if force.is_pressed():
+    if (timer < timer.now()):
+        distance_sensor.wait_for_distance_closer_than(1, 'in')
         color = color_sensor.get_color()
         if color == 'red':
             liftArm(1)
@@ -58,13 +79,16 @@ def targetSearch():
         else:
             hub.speaker.beep(60, 0.5)
             rowSearch()
-
+        
 
         
 
-def searchAndRescue():
-    targetSearch()
-    spinSearch()
-    rowSearch()
+# def searchAndRescue():
+#     spinSearch()
+#     targetSearch()
+#     rowSearch()
 
 
+# searchAndRescue()
+
+spinSearch()
